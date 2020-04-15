9c146919ea177cbf81ac52dbf7253eb514e7dcc2
Terrence Wong <wongte@oocl.com>
2020-04-15 19:15:06 +0800

    [Terrence] - add create/update/delete company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -49,5 +50,28 @@ public class CompanyController {
                 .findAny();
 
         return targetCompany.map(Company::getEmployees).orElse(null);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company create(@RequestBody Company newCompany) {
+        companies.add(newCompany);
+        return newCompany;
+    }
+
+    @DeleteMapping("/{targetCompanyID}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void delete(@PathVariable int targetCompanyID) {
+        companies.removeIf(company -> company.getCompanyID() == targetCompanyID);
+    }
+
+    @PutMapping("/{targetCompanyID}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void update(@PathVariable Integer targetCompanyID, @RequestBody Company updatedCompany) {
+        for (int index = 0; index < companies.size(); index++) {
+            if (companies.get(index).getCompanyID() == targetCompanyID) {
+                companies.set(index, updatedCompany);
+            }
+        }
     }
 }
