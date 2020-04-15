a8d2497aba61895c1642e964c982f572eb01957c
Terrence Wong <wongte@oocl.com>
2020-04-15 17:25:17 +0800

    [Terrence] - complete get all employees function

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("employees")
+public class EmployeeController {
+    private List<Employee> employees;
+
+    public EmployeeController() {
+        employees = new ArrayList<>();
+        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
+        employees.add(new Employee(1, "Xiaohong", 19, "Female"));
+        employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
+        employees.add(new Employee(3, "Xiaogang", 16, "Male"));
+        employees.add(new Employee(4, "Xiaoxia", 15, "Female"));
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employees;
+    }
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.springbootemployee.model;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Employee(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
